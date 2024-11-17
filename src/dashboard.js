36039d937521197f872332cbd49d7670d58e77c4
Akar <akarsihar@gmail.com>
2024-11-17 11:09:20 +0530

    total saled product

---
+++ src/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TotalSalesProduct from './components/TotalSalesProduct'
+
+function Dashboard() {
+  return (
+    <div className='flex'>
+      <TotalSalesProduct />
+      <TotalSalesProduct />
+
+    </div>
+  )
+}
+
+export default Dashboard
